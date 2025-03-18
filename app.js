23db06785137eeeaf8cb393d22d42824d3239fab
DCVICE <daivisgabriel@gmail.com>
2025-03-17 22:30:28 -0400

    Update app.js
    agregue el código

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arrayAmigos = [];
+
+function agregarAmigo() {
+    let name = document.getElementById('amigo').value;
+    let lista = document.getElementById('listaAmigos');
+
+    if (name.trim() !== '') {
+        let nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = name;
+        lista.appendChild(nuevoElemento);
+        arrayAmigos.push(name);
+    } else {
+        alert("Ingresar un nombre");
+    }
+    document.getElementById('amigo').value = "";
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = "";
+}
+
+function sortearAmigo() {
+    let resultado = document.getElementById('resultado');
+
+    if (arrayAmigos.length != 0) {
+        let numeroRandom = Math.floor(Math.random() * arrayAmigos.length)
+        let nombreRandom = arrayAmigos[numeroRandom];
+        let elementoResultado = document.createElement('li');
+        elementoResultado.textContent = "El nombre aleatorio: " + nombreRandom;
+        resultado.appendChild(elementoResultado);
+
+        let lista = document.getElementById('listaAmigos');
+        lista.innerHTML = "";
+        arrayAmigos = [];
+    } else {
+        alert("La lista esta vacia");
+    }
+}
